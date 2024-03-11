966321e866cf8260b2b01565bc8d11a0739156be
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-03-11 12:10:15 +0100

    color schemes updated

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}", // add this line
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#019DDE',
+        yellow: '#FFE454',
+        red: '#E86A6A',
+        green: '#4EBE6D',
+        heading: '#1D242E',
+        accent: '#F5F8FF',
+        dashboard: '#FBF9F9',
+        hover: '#26baf8',
+        secondary: '#24C9FA',
+        text: '#747272'
+      }
+    },
   },
   plugins: [],
 }
